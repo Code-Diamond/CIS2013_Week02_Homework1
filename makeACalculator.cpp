fce24399771e76918133a69eea85aed4127e05cf
JaminROCK <KingJamin@hotmail.com>
2018-02-12 19:58:15 -0600

    Update makeACalculator.cpp
    Code modularized; primed to have more added on to it.

--- makeACalculator.cpp
+++ makeACalculator.cpp
@@ -2,9 +2,53 @@
 #include <string>
 using namespace std;
 
+//global variables
+
+char continuation, condition, operation;
+int x, y;
+
+void calculate()
+{
+    switch (operation)
+    {
+        case '+':
+            cout << x+y;
+            break;
+        case '-':
+            cout << x-y;
+            break;
+        case '/':
+            cout << x/y;
+            break;
+        case '*':
+            cout << x*y;
+            break;
+    }
+    
+}
+
+//Portable console clearing operation.
+void continueAndClear()
+{
+
+    if(continuation == 'y' || continuation == 'Y')
+    {
+        condition = true;
+        cout << string( 100, '\n' );
+
+    }
+    else
+    {
+        //If user types anything other than y or capital Y then simply exit.
+        condition = false;
+        cout << string( 100, '\n' );
+    }
+}
+
+
 int main()
 {
-    bool condition = true;
+    condition = true;
     
     //TO:DO
         //string calculatorType;
@@ -13,52 +57,24 @@ int main()
     while(condition)
     {
         //Retrieve the user's operation in the form of a char datatype.
-        char operation;
         cout << "Do you want to do addition, division, subtraction, or multiplication?\n Type \"/\", \"*\", \"-\", or \"+\": ";
     	cin >> operation;
     
-    	int x;
-    	int y;
         //Retrieve x and y values from user.
-        cout << "Now type the first number (x): "; cin >> x;
-        cout << "Now type the second number (y): "; cin >> y;
+        cout << "Now type the first number (x): ";
+        cin >> x;
+        cout << "Now type the second number (y): "; 
+        cin >> y;
     
         //Determine the user's math operation through a switch.
-    	switch (operation)
-    	{
-    		case '+':
-    			cout << x+y;
-    			break;
-    		case '-':
-    			cout << x-y;
-    			break;
-    		case '/':
-    			cout << x/y;
-    			break;
-    		case '*':
-    			cout << x*y;
-    			break;
-    	}
-    	
+        calculate();
+
     	//Ask user if they want to continue.
-    	char continuation;
-    	cout << "\nWould you like to continue? (y/n): "; cin >> continuation;
+    	cout << "\nWould you like to continue? (y/n): "; 
+        cin >> continuation;
     	
-    	
-    	//Portable console clearing operation.
-    	if(continuation == 'y' || continuation == 'Y')
-    	{
-    	    condition = true;
-    	    cout << string( 100, '\n' );
+    	continueAndClear();
 
-    	}
-    	else
-    	{
-    	    //If user types anything other than y or capital Y then simply exit.
-    	    condition = false;
-    	    cout << string( 100, '\n' );
-
-    	}
     	
     	
     }
